95734b7f630e9f59cd7adcef66a2f49ba0e2ed8f
Adam Fraser <adam.fraser@gmail.com>
2021-06-02 19:16:18 -0700

    add support for scala 3 (#17)

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -17,63 +17,69 @@ object BuildHelper {
     list.map(v => (v.split('.').take(2).mkString("."), v)).toMap
   }
 
-  private val Scala212 = versions("2.12")
-  private val Scala213 = versions("2.13")
+  private val Scala212   = versions("2.12")
+  private val Scala213   = versions("2.13")
+  private val ScalaDotty = versions("3.0")
 
   private val SilencerVersion = "1.7.5"
 
   private val stdOptions = Seq(
+    "-deprecation",
     "-encoding",
     "UTF-8",
-    "-explaintypes",
-    "-Yrangepos",
     "-feature",
-    "-language:higherKinds",
-    "-language:existentials",
-    "-Xlint:_,-type-parameter-shadow",
-    "-Xsource:2.13",
-    "-Ywarn-numeric-widen",
-    "-Ywarn-value-discard",
     "-unchecked",
-    "-deprecation",
     "-Xfatal-warnings"
   )
 
-  private val stdOpts213 = Seq(
-    "-Wunused:imports",
-    "-Wvalue-discard",
-    "-Wunused:patvars",
-    "-Wunused:privates",
-    "-Wunused:params",
-    "-Wvalue-discard"
+  private val std2xOptions = Seq(
+    "-language:higherKinds",
+    "-language:existentials",
+    "-explaintypes",
+    "-Yrangepos",
+    "-Xlint:_,-missing-interpolator,-type-parameter-shadow",
+    "-Ywarn-numeric-widen",
+    "-Ywarn-value-discard"
   )
 
-  private val stdOptsUpto212 = Seq(
-    "-Xfuture",
-    "-Ypartial-unification",
-    "-Ywarn-nullary-override",
-    "-Yno-adapted-args",
-    "-Ywarn-infer-any",
-    "-Ywarn-inaccessible",
-    "-Ywarn-nullary-unit",
-    "-Ywarn-unused-import"
-  )
+  private def optimizerOptions(optimize: Boolean) =
+    if (optimize)
+      Seq(
+        "-opt:l:inline",
+        "-opt-inline-from:zio.internal.**"
+      )
+    else Nil
 
-  private def extraOptions(scalaVersion: String) =
+  def extraOptions(scalaVersion: String, optimize: Boolean) =
     CrossVersion.partialVersion(scalaVersion) match {
+      case Some((3, 0)) =>
+        Seq(
+          "-language:implicitConversions",
+          "-Xignore-scala2-macros"
+        )
       case Some((2, 13)) =>
-        stdOpts213
+        Seq(
+          "-Ywarn-unused:params,-implicits"
+        ) ++ std2xOptions ++ optimizerOptions(optimize)
       case Some((2, 12)) =>
         Seq(
           "-opt-warnings",
           "-Ywarn-extra-implicit",
           "-Ywarn-unused:_,imports",
           "-Ywarn-unused:imports",
-          "-opt:l:inline",
-          "-opt-inline-from:<source>"
-        ) ++ stdOptsUpto212
-      case _ =>
-        Seq("-Xexperimental") ++ stdOptsUpto212
+          "-Ypartial-unification",
+          "-Yno-adapted-args",
+          "-Ywarn-inaccessible",
+          "-Ywarn-infer-any",
+          "-Ywarn-nullary-override",
+          "-Ywarn-nullary-unit",
+          "-Ywarn-unused:params,-implicits",
+          "-Xfuture",
+          "-Xsource:2.13",
+          "-Xmax-classfile-name",
+          "242"
+        ) ++ std2xOptions ++ optimizerOptions(optimize)
+      case _ => Seq.empty
     }
 
   def buildInfoSettings(packageName: String) =
@@ -85,16 +91,20 @@ object BuildHelper {
 
   def stdSettings(prjName: String) = Seq(
     name := s"$prjName",
-    crossScalaVersions := Seq(Scala212, Scala213),
+    crossScalaVersions := Seq(Scala212, Scala213, ScalaDotty),
     ThisBuild / scalaVersion := Scala212,
-    scalacOptions := stdOptions ++ extraOptions(scalaVersion.value),
-    libraryDependencies ++=
-      Seq(
-        ("com.github.ghik" % "silencer-lib" % SilencerVersion % Provided)
-          .cross(CrossVersion.full),
-        compilerPlugin(("com.github.ghik" % "silencer-plugin" % SilencerVersion).cross(CrossVersion.full)),
-        compilerPlugin("org.typelevel" %% "kind-projector" % "0.10.3")
-      ),
+    scalacOptions := stdOptions ++ extraOptions(scalaVersion.value, optimize = !isSnapshot.value),
+    libraryDependencies ++= {
+      if (scalaVersion.value == ScalaDotty)
+        Seq(
+          "com.github.ghik" % s"silencer-lib_$Scala213" % SilencerVersion % Provided
+        )
+      else
+        Seq(
+          "com.github.ghik" % "silencer-lib" % SilencerVersion % Provided cross CrossVersion.full,
+          compilerPlugin("com.github.ghik" % "silencer-plugin" % SilencerVersion cross CrossVersion.full)
+        )
+    },
     incOptions ~= (_.withLogRecompileOnMacro(false))
   )
 }
