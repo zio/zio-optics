dbd85f8549b596c044341d04519713b2f6e31642
Adam Fraser <adam.fraser@gmail.com>
2021-06-04 13:39:10 -0700

    add optic syntax

--- src/main/scala/zio/optics/Optics.scala
+++ src/main/scala/zio/optics/Optics.scala
@@ -5,4 +5,18 @@ trait Optics
     with OpticFailureModule
     with OpticModule
     with OpticResultModule
-    with OpticTypesModule
+    with OpticTypesModule {
+
+  /**
+   * Provides implicit syntax for working with any value as a partially
+   * applied optic.
+   */
+  implicit final class OpticSyntax[Whole](private val self: Whole) {
+
+    /**
+     * Views this value as a partially applied optic.
+     */
+    def optic: OpticPartiallyApplied[Whole, Nothing, Nothing, Whole, Whole] =
+      Optic.identity(self)
+  }
+}
--- src/test/scala/zio/optics/TraversalSpec.scala
+++ src/test/scala/zio/optics/TraversalSpec.scala
@@ -4,14 +4,13 @@ import zio._
 import zio.test._
 import zio.test.Assertion._
 
-object TraversalSpec extends DefaultRunnableSpec {
+object TraversalSpec extends DefaultRunnableSpec with EitherCompat {
 
   def spec = suite("TraversalSpec")(
     suite("operators")(
       testM("update") {
         check(Gen.chunkOf(Gen.either(Gen.anyInt, Gen.anyInt)), Gen.function(Gen.anyInt)) { (chunk, f) =>
-          val rights   = Optic.identity[Chunk[Either[Int, Int]]].foreach(Prism.right)
-          val actual   = rights.update(chunk)(_.map(f))
+          val actual   = chunk.optic.foreach(Prism.right).update(_.map(f))
           val expected = chunk.map(_.map(f))
           assert(actual)(isRight(equalTo(expected)))
         }
